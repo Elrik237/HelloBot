664c582bb46e05c2ae5941f630c6b8863f625445
Эдуард Поляков <76116843+Elrik237@users.noreply.github.com>
2020-12-18 18:42:56 +0300

    Подключил бота @Dodya_start_bot в Telegram
    ###Теперь он может:
    - Отвечать на /start
    - Повторять все получаемые им сообщения
    - Отвечать CAPS, по команде /caps

--- main.py
+++ main.py
@@ -1,17 +1,65 @@
 import telegram
-from telegram.ext import Updater
-import logging
-import datetime
-
 bot = telegram.Bot(token='1440425473:AAGgDLB3-dihOgOagG5mVfktEzJVhWEDc0g')
+
+from telegram.ext import Updater
 updater = Updater(token='1440425473:AAGgDLB3-dihOgOagG5mVfktEzJVhWEDc0g', use_context=True)
 dispatcher = updater.dispatcher
+
+import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
+
+from telegram.ext import CommandHandler
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, я бот и только начинаю "
                                                                     "развиваться. В будущем я захвачу "
                                                                     "человечество!!!")
 
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+from telegram.ext import InlineQueryHandler
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+updater.stop()
+
+
+import datetime
 print(datetime.datetime.today().strftime('%m/%d/%Y %H:%M'))
