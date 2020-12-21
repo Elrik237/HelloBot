ce7a538ac35f5e0df361768c61b98882baf9e924
Эдуард Поляков <76116843+Elrik237@users.noreply.github.com>
2020-12-21 11:43:39 +0300

    Отформатировал код

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import telegram
+from telegram.ext import Updater
+from telegram.ext import InlineQueryHandler
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import logging
+import datetime
+
 bot = telegram.Bot(token='1440425473:AAGgDLB3-dihOgOagG5mVfktEzJVhWEDc0g')
 
-from telegram.ext import Updater
+
 updater = Updater(token='1440425473:AAGgDLB3-dihOgOagG5mVfktEzJVhWEDc0g', use_context=True)
 dispatcher = updater.dispatcher
 
-import logging
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-from telegram.ext import CommandHandler
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, я бот и только начинаю "
                                                                     "развиваться. В будущем я захвачу "
@@ -18,22 +24,22 @@ def start(update, context):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+time = datetime.datetime.today().strftime('%m/%d/%Y %H:%M')
 
 def echo(update, context):
     time = datetime.datetime.today().strftime('%m/%d/%Y %H:%M')
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text + ' ' + time)
 
-from telegram.ext import MessageHandler, Filters
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
 
-import datetime
-time = datetime.datetime.today().strftime('%m/%d/%Y %H:%M')
+
 def time_now(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=time)
 
 time_now_handler = CommandHandler('time_now', time_now)
 dispatcher.add_handler(time_now_handler)
+
 
 def inline_time_now(update, context):
     query = update.inline_query.query
@@ -49,9 +55,9 @@ def inline_time_now(update, context):
     )
     context.bot.answer_inline_query(update.inline_query.id, results)
 
-from telegram.ext import InlineQueryHandler
 inline_time_now_handler = InlineQueryHandler(inline_time_now)
 dispatcher.add_handler(inline_time_now_handler)
+
 
 def caps(update, context):
     text_caps = ' '.join(context.args).upper()
@@ -60,11 +66,13 @@ def caps(update, context):
 caps_handler = CommandHandler('caps', caps)
 dispatcher.add_handler(caps_handler)
 
+
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
+
 
 updater.start_polling()
 updater.idle()
