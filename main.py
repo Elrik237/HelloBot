c6d8f3a4e81c3f3ea0322e15baf582fe7ab23699
Эдуард Поляков <76116843+Elrik237@users.noreply.github.com>
2020-12-21 21:10:36 +0300

    Отформатировал код

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from telegram.ext import Updater, CommandHandler,InlineQueryHandler, MessageHand
 from all_functions import start, time_now, caps, echo, unknown, inline_time_now
 import os
 
-updater = Updater(token=os.environ['token_bot'], use_context=True)
-dispatcher = updater.dispatcher
+def main():
+    updater = Updater(token=os.environ['token_bot'], use_context=True)
+    dispatcher = updater.dispatcher
 
-dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CommandHandler('time_now', time_now))
-dispatcher.add_handler(CommandHandler('caps', caps))
-dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
-dispatcher.add_handler(MessageHandler(Filters.command, unknown))
-dispatcher.add_handler(InlineQueryHandler(inline_time_now))
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('time_now', time_now))
+    dispatcher.add_handler(CommandHandler('caps', caps))
+    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+    dispatcher.add_handler(InlineQueryHandler(inline_time_now))
 
-updater.start_polling()
-updater.idle()
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
